66b00a0342ceae4336630ea884649cf5dfb0c19a
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-25 17:44:33 -0400

    clear function added clear function

--- javascript.js
+++ javascript.js
@@ -31,10 +31,14 @@ const opBtns = ops.querySelectorAll('button');
 opBtns.forEach((button) => {
     if (button.innerText !== '=') {
         button.addEventListener('click', () => {
-            operator = button.innerText;
-            boolean = true;
-            console.log(operator);
-            updateDisplay(operator);
+            if (boolean == false) {
+                operator = button.innerText;
+                boolean = true;
+                console.log(operator);
+                updateDisplay(operator);
+            } else if (boolean == true) {
+                operate(num1, num2, operator);
+            }
     });
     }
 });
@@ -42,12 +46,19 @@ opBtns.forEach((button) => {
 //for equals
 const equalBtn = document.getElementById('equalBtn');
 equalBtn.addEventListener('click', () => {
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
     operate(num1, num2, operator);
     console.log(num1, num2, operator);
 });
 
+//clear button
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.addEventListener('click', () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    boolean = false;
+    display.innerHTML = '';
+});
 
 //functions
 function updateDisplay(value) {
@@ -56,6 +67,8 @@ function updateDisplay(value) {
 }
 
 function operate(a, b, op) {
+    a = parseInt(a);
+    b = parseInt(b);
     if (op === '+') {
         return display.innerHTML = add(a, b);
     } else if (op === '-') {
@@ -68,8 +81,11 @@ function operate(a, b, op) {
 }
 
 function add(a, b) {
-    console.log('add these two numbers for: ' + a + b);
-    return parseInt(a + b);
+    num1 = a + b;
+    operator = '';
+    num2 = '';
+    boolean = false;
+    return (a + b);
 }
 
 function subtract(a, b) {
