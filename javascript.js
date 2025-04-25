788dc8925f08d371cbd9668e8fc3b543f1f70cf0
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-25 11:59:00 -0400

    created variales created 3 variables for each num and the opperator

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
-function add(a, b) {
-    return a + b;
+//variables
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+
+//functions
+
+function add(num1, num2) {
+    return num1 + num2;
 }
 
 function subtract(a, b) {
