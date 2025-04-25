339a8b1933d9f35813a8da329855495fe6c498a3
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-25 14:29:56 -0400

    button event listeners event listeners for buttons to update display

--- javascript.js
+++ javascript.js
@@ -2,8 +2,26 @@
 let num1 = 5;
 let operator = '+';
 let num2 = 5;
+const display = document.querySelector('h3');
+display.innerHTML = ' ';
+//button setup
+const numbers = document.getElementById('numBtns');
+const numBtns = numbers.querySelectorAll('button');
+numBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        const btnId = button.innerText;
+        console.log(btnId);
+        updateDisplay(btnId);
+    });
+});
+
 
 //functions
+function updateDisplay(value) {
+    let currentDisplay = display.innerHTML;
+    display.innerHTML = currentDisplay + value;
+}
+
 function operate(a, b, op) {
     if (op === '+') {
         return add(a, b);
